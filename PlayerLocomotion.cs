7eef97a2984e37d9db9611c54f1e154ee8f9d439
KyrieRui <37173293+KyrieRui@users.noreply.github.com>
2021-11-09 14:03:41 +1300

    Assets
    This folder is for me to storing all my C# scripts and Animations

---
+++ PlayerLocomotion.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+    public class PlayerLocomotion : MonoBehaviour
+    {
+        Transform cameraObject;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
+
+        [HideInInspector]
+        public Transform myTransform;
+        [HideInInspector]
+        public AnimatorHandler animatorHandlor;
+
+        public new Rigidbody rigidbody;
+        public GameObject normalCamera;
+
+        [Header("Stats")]
+        [SerializeField]
+        float movementSpeed = 5;
+        [SerializeField]
+        float sprintSpeed = 7;
+        [SerializeField]
+        float rotationSpeed = 10;
+
+        public bool isSprinting;
+
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            animatorHandlor = GetComponentInChildren<AnimatorHandler>();
+            cameraObject = Camera.main.transform;
+            myTransform = transform;
+            animatorHandlor.Initialize();
+
+
+        }
+        public void Update()
+        {
+            float delta = Time.deltaTime;
+
+
+            isSprinting = inputHandler.b_Input;
+            inputHandler.TickInput(delta);
+            HandleMovent(delta);
+            HandleRollingAndSprinting(delta);
+
+        }
+
+        #region Movement
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDir = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+
+            targetDir = cameraObject.forward * inputHandler.vertical;
+            targetDir += cameraObject.right * inputHandler.horizontal;
+
+            targetDir.Normalize();
+            targetDir.y = 0;
+
+            if (targetDir == Vector3.zero)
+                targetDir = myTransform.forward;
+
+            float rs = rotationSpeed;
+
+            Quaternion tr = Quaternion.LookRotation(targetDir);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+        
+        public void HandleMovent(float delta)
+        {
+            if (inputHandler.rollFlag)
+                return;
+
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+            moveDirection.y = 0;
+
+            float speed = movementSpeed;
+
+            if(inputHandler.sprintFlag)
+            {
+                speed = sprintSpeed;
+                isSprinting = true;
+                moveDirection *= speed;
+            }
+            else
+            {
+                moveDirection *= speed;
+            }
+           
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidbody.velocity = projectedVelocity;
+
+            animatorHandlor.UpdateAnimatorValues(inputHandler.moveAmount, 0,  isSprinting);
+
+            if (animatorHandlor.canRotate)
+            {
+                HandleRotation(delta);
+            }
+        }
+
+        public void HandleRollingAndSprinting(float delta)
+        {
+            if (animatorHandlor.anim.GetBool("isInteracting"))
+                return;
+            if(inputHandler.rollFlag)
+            {
+                moveDirection = cameraObject.forward * inputHandler.vertical;
+                moveDirection += cameraObject.right * inputHandler.horizontal;
+
+                if(inputHandler.moveAmount > 0)
+                {
+                    animatorHandlor.PlayerTargetAnimation("Rolling", true);
+                    moveDirection.y = 0;
+                    Quaternion rollRotation = Quaternion.LookRotation(moveDirection);
+                    myTransform.rotation = rollRotation;
+                }
+                else
+                {
+                    animatorHandlor.PlayerTargetAnimation("Backstep", true);
+                }
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+        Animator anim;
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+        }
+
+        void Update()
+        {
+            inputHandler.isInteracting = anim.GetBool("isInteracting");
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+        }
+    }
+}
